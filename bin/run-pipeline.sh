b48c1e3e0335fd9d4d5ff1b22a33ca617a2f6448
Sheila Sagear <ssagearastro@gmail.com>
2025-06-06 14:49:06 -0400

    add importance run to pipeline script

--- bin/run-pipeline.sh
+++ bin/run-pipeline.sh
@@ -9,3 +9,8 @@ cd ../
 python detrend_and_estimate_ttvs.py --mission 'Kepler' --target $1 --project_dir './' --data_dir 'Data/' --catalog 'cumulative_2025.05.30_14.09.29.csv' --run_id '06_01_25'
 python analyze_autocorrelated_noise.py --mission 'Kepler' --target $1 --project_dir './' --data_dir 'Data/' --catalog 'cumulative_2025.05.30_14.09.29.csv' --run_id '06_01_25'
 python fit_transit_shape_simultaneous_nested.py --mission 'Kepler' --target $1 --project_dir './' --data_dir 'Data/' --catalog 'cumulative_2025.05.30_14.09.29.csv' --run_id '06_01_25'
+python importance.py $1 0
+python importance.py $1 1
+python importance.py $1 2
+python importance.py $1 3
+python importance.py $1 4
